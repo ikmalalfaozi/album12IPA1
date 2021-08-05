641d33a6ad8d38a0724c0d84708872ee19d680db
ikmalalfaozi <16520205@std.stei.itb.ac.id>
2021-08-05 11:38:10 +0700

    memperbarui tampilan website

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const container = document.querySelector('.container')
-const jumbo = document.querySelector('.jumbo');
+const container = document.querySelector('.container');
+const layer = document.querySelector('.layer');
+const close = document.querySelector('.close');
+const image = document.querySelector('.image');
 
 container.addEventListener('click', function(e) {
 	
 	// cek apakah yang diklik adalah thumb
 	if (e.target.className == "thumb") {
-		jumbo.src = e.target.src;
-		jumbo.classList.add('fade');
-		setTimeout(function() {
-			jumbo.classList.remove('fade');
-		}, 500);
+		layer.classList.add('overlay');
+		close.src = "close.png";
+		image.classList.add('besar');
+		image.src = e.target.src;
 	}
+	
+	if (e.target.className == "close") {
+		layer.classList.remove('overlay');
+		close.src = "";
+		image.classList.remove('besar');
+		image.src = "";
+	}
+	//jumbo.src = e.target.src;
+	//jumbo.classList.add('fade');
+	//setTimeout(function() {
+	//	jumbo.classList.remove('fade');
+	//}, 500);
 });
